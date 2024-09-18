8dcd09e73db6249aa0512ed36c4dc95a1fd1d9cb
Lidiane Trindade <47565259+lstrindade@users.noreply.github.com>
2024-09-18 14:29:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+
+movies_data = pd.read_csv('final.csv')
+
+app = Flask(__name__)
+
+all_movies = movies_data[["original_title","poster_link","release_date","runtime","weighted_rating"]]
+
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
